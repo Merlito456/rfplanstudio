5bd7e031418975232835fa0a7f22798f98c2666c
Merlito456 <rabanes.johncarlo1994@gmail.com>
2026-01-17 23:23:46 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -9,8 +9,5 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Removed StrictMode as it causes double-initialization issues with Leaflet
+root.render(<App />);
